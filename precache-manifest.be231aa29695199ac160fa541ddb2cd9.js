87c8c4afced9bc9c22aa725f880613dccb54828d
Евгений Ращупкин <eugenerndev@gmail.com>
2019-09-09 16:17:59 +0400

    Updates

---
+++ precache-manifest.be231aa29695199ac160fa541ddb2cd9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b71db06b693f1ada6ccee3b892b3740d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "011b30a1aef30643f689",
+    "url": "/static/css/main.cb58d331.chunk.css"
+  },
+  {
+    "revision": "6a7399c8a13328491662",
+    "url": "/static/js/2.a89e8c92.chunk.js"
+  },
+  {
+    "revision": "011b30a1aef30643f689",
+    "url": "/static/js/main.0baf4b66.chunk.js"
+  },
+  {
+    "revision": "14cd8008c46d86bb1af0",
+    "url": "/static/js/runtime~main.fef71986.js"
+  },
+  {
+    "revision": "24b0711419d9c6cadabcb2ac4f20cc75",
+    "url": "/static/media/spin.24b07114.svg"
+  }
+]);
